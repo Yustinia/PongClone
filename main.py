1b61182ed2f7b07555399f72f6c003c75d923556
Yustinia <yustiniayevsa+development@gmail.com>
2025-11-02 19:52:11 +0800

    refactor: converted l-r movement to Player class

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ bg_rect = bg_surf.get_rect(center=(SCREEN_WD / 2, SCREEN_HT / 2))
 bg_surf.fill((25, 25, 25))
 
 top_bor_surf = pygame.Surface((SCREEN_WD, 50))
-top_rect = top_bor_surf.get_rect(midtop=(SCREEN_WD / 2, 0))
+top_bor_rect = top_bor_surf.get_rect(midtop=(SCREEN_WD / 2, 0))
 top_bor_surf.fill((255, 255, 255))
 
 bottom_bor_surf = pygame.Surface((SCREEN_WD, 50))
-bottom_rect = bottom_bor_surf.get_rect(midbottom=(SCREEN_WD / 2, 600))
+bottom_bor_rect = bottom_bor_surf.get_rect(midbottom=(SCREEN_WD / 2, 600))
 bottom_bor_surf.fill((255, 255, 255))
 
 center_line_surf = pygame.Surface((10, 450))
@@ -34,6 +34,41 @@ right_player_rect = right_player_surf.get_rect(midright=(790, SCREEN_HT / 2))
 right_player_surf.fill((255, 255, 255))
 
 
+class Player:
+    def __init__(
+        self,
+        surface,
+        rect,
+        bordertop=top_bor_rect,
+        borderbottom=bottom_bor_rect,
+        speed: int = 10,
+    ) -> None:
+        self.surface = surface
+        self.rect = rect
+        self.bordertop = bordertop
+        self.borderbottom = borderbottom
+        self.speed = speed
+
+    def movement(self, keypress):
+        if keypress[pygame.K_w] or keypress[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keypress[pygame.K_s] or keypress[pygame.K_DOWN]:
+            self.rect.y += self.speed
+
+    def collision(self):
+        if self.rect.bottom >= self.borderbottom.top:
+            self.rect.bottom = self.borderbottom.top
+        if self.rect.top <= self.bordertop.bottom:
+            self.rect.top = self.bordertop.bottom
+
+    def update(self, keypress):
+        self.movement(keypress)
+        self.collision()
+
+
+l_player = Player(left_player_surf, left_player_rect)
+r_player = Player(right_player_surf, right_player_rect)
+
 # set state
 run_state = True
 
@@ -42,25 +77,18 @@ while run_state:
         if event.type == pygame.QUIT:
             run_state = False
 
-    key_press = pygame.key.get_pressed()
-    if key_press[pygame.K_w]:
-        left_player_rect.y -= 5
-    if key_press[pygame.K_s]:
-        left_player_rect.y += 5
+    keys = pygame.key.get_pressed()
+
+    l_player.update(keys)
+    r_player.update(keys)
 
     screen.blit(bg_surf, bg_rect)
-    screen.blit(top_bor_surf, top_rect)
-    screen.blit(bottom_bor_surf, bottom_rect)
+    screen.blit(top_bor_surf, top_bor_rect)
+    screen.blit(bottom_bor_surf, bottom_bor_rect)
     screen.blit(center_line_surf, center_line_rect)
 
     screen.blit(left_player_surf, left_player_rect)
     screen.blit(right_player_surf, right_player_rect)
-
-    # set border collision
-    if left_player_rect.bottom >= bottom_rect.top:
-        left_player_rect.bottom = bottom_rect.top
-    if left_player_rect.top <= top_rect.bottom:
-        left_player_rect.top = top_rect.bottom
 
     clock.tick(60)
     pygame.display.update()
